cbb36ec23de1902e84237ada36c1de8799ee0b86
max4805 <max480@free.fr>
2023-03-11 23:44:20 +0100

    Add MadelineBrownHair

---
+++ MadelineBrownHair/MadelineBrownHairModule.cs
@@ -0,0 +1,60 @@
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.BadelineHairDye {
+
+    public class MadelineBrownHairModule : EverestModule {
+
+        private static Color OneDashColor = Calc.HexToColor("654321");
+
+        private static ParticleType P_DashB;
+
+        public override void Load() {
+            On.Celeste.Player.GetCurrentTrailColor += onPlayerGetTrailColor;
+            On.Celeste.Player.UpdateHair += onPlayerUpdateHair;
+            On.Celeste.Player.DashUpdate += onPlayerDashUpdate;
+        }
+
+        public override void LoadContent(bool firstLoad) {
+            base.LoadContent(firstLoad);
+
+            P_DashB = new ParticleType(Player.P_DashA) {
+                Color = OneDashColor,
+                Color2 = Calc.HexToColor("82562A")
+            };
+        }
+
+        public override void Unload() {
+            On.Celeste.Player.GetCurrentTrailColor -= onPlayerGetTrailColor;
+            On.Celeste.Player.UpdateHair -= onPlayerUpdateHair;
+            On.Celeste.Player.DashUpdate -= onPlayerDashUpdate;
+        }
+
+        private Color onPlayerGetTrailColor(On.Celeste.Player.orig_GetCurrentTrailColor orig, Player self) {
+            if (self.Dashes == 1) {
+                return OneDashColor;
+            }
+            return orig(self);
+        }
+
+        private void onPlayerUpdateHair(On.Celeste.Player.orig_UpdateHair orig, Player self, bool applyGravity) {
+            orig(self, applyGravity);
+            if (self.Dashes == 1) {
+                self.Hair.Color = OneDashColor;
+            }
+        }
+
+        private int onPlayerDashUpdate(On.Celeste.Player.orig_DashUpdate orig, Player self) {
+            ParticleType p_DashB = Player.P_DashB;
+            ParticleType p_DashBadB = Player.P_DashBadB;
+            Player.P_DashB = P_DashB;
+            Player.P_DashBadB = P_DashB;
+
+            int result = orig.Invoke(self);
+
+            Player.P_DashB = p_DashB;
+            Player.P_DashBadB = p_DashBadB;
+            return result;
+        }
+    }
+}
