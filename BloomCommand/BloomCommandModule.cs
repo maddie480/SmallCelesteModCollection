679b1423cf1a32a62d0ee3a227b70309393ec98d
max4805 <max480@free.fr>
2023-03-11 22:17:46 +0100

    Add Bloom Command

---
+++ BloomCommand/BloomCommandModule.cs
@@ -0,0 +1,17 @@
+﻿using Monocle;
+
+namespace Celeste.Mod.BloomCommand {
+    public class BloomCommandModule : EverestModule {
+        public override void Load() { }
+
+        public override void Unload() { }
+
+        [Command("bloom", "checks bloom values")]
+        private static void CmdBloom() {
+            Level level = Engine.Scene as Level;
+            if (level != null) {
+                Engine.Commands.Log("level base(" + AreaData.Get(level).BloomBase + "), session base add(" + level.Session.BloomBaseAdd + "), current base(" + level.Bloom.Base + "), current strength(" + level.Bloom.Strength + ")");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 A repository collecting all my smaller or map-specific code mods for Celeste.
 
 Here is the list:
+- [Bloom Command](https://gamebanana.com/mods/53703) - a mod that adds a `bloom` console command similar to the vanilla `lighting` command
 - [Event Mode](https://gamebanana.com/mods/53674) - a tiny mod that puts Celeste in a hidden "event mode"
 - [Full Clear Stamp](https://gamebanana.com/mods/34279) - a mod adding a stamp on a save file for full-clearing a campaign
 - [Golden Bird in Journal](https://gamebanana.com/mods/53662) - a mod turning the bird in your journal golden when you full clear Farewell
