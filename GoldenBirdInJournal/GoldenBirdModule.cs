54989e8b636824e54e0a0b7dfd2e12e2ea51ca6b
max4805 <max480@free.fr>
2023-03-11 21:31:36 +0100

    Add Golden Bird in Journal

---
+++ GoldenBirdInJournal/GoldenBirdModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using On.Celeste;
+
+namespace Celeste.Mod.GoldenBird {
+    public class GoldenBirdModule : EverestModule {
+
+        public override Type SettingsType => null;
+
+        public override void Load() {
+            On.Celeste.OuiJournalProgress.CompletionIcon += ModCompletionIcon;
+        }
+
+        public override void Unload() {
+            On.Celeste.OuiJournalProgress.CompletionIcon -= ModCompletionIcon;
+        }
+
+        private string ModCompletionIcon(On.Celeste.OuiJournalProgress.orig_CompletionIcon orig, OuiJournalProgress self, AreaStats data) {
+            if(!AreaData.Get(data.ID).CanFullClear && data.Modes[0].SingleRunCompleted) {
+                foreach (EntityData strawberry in AreaData.Areas[data.ID].Mode[0].MapData.Strawberries) {
+                    EntityID item = new EntityID(strawberry.Level.Name, strawberry.ID);
+                    if (strawberry.Bool("moon", false) && data.Modes[0].Strawberries.Contains(item)) {
+                        return "max480/goldenbird/goldenbird";
+                    }
+                }
+            }
+            return orig(self, data);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ A repository collecting all my smaller or map-specific code mods for Celeste.
 
 Here is the list:
 - [Full Clear Stamp](https://gamebanana.com/mods/34279) - a mod adding a stamp on a save file for full-clearing a campaign
+- [Golden Bird in Journal](https://gamebanana.com/mods/53662) - a mod turning the bird in your journal golden when you full clear Farewell
