04f3820d859807efcfcbb512674d043905573805
max4805 <max480@free.fr>
2023-04-01 14:29:23 +0200

    Add Crabbing Antidote

---
+++ CrabbingAntidote/CrabbingAntidoteModule.cs
@@ -0,0 +1,34 @@
+using Celeste;
+using Celeste.Mod;
+using System;
+
+namespace Celeste.Mod.CrabbingAntidote {
+    public class CrabbingAntidoteModule : EverestModule {
+        public override void Load() {
+            On.Celeste.SaveData.AfterInitialize += onSaveDataAfterInitialize;
+        }
+
+        public override void Unload() {
+            On.Celeste.SaveData.AfterInitialize -= onSaveDataAfterInitialize;
+        }
+
+        private void onSaveDataAfterInitialize(On.Celeste.SaveData.orig_AfterInitialize orig, SaveData self) {
+            orig(self);
+
+            foreach (AreaStats areaStats in self.Areas) {
+                if (areaStats.Modes.Length < 3) {
+                    AreaModeStats[] decrabbedStats = new AreaModeStats[3];
+
+                    for (int i = 0; i < areaStats.Modes.Length; i++) {
+                        decrabbedStats[i] = areaStats.Modes[i];
+                    }
+                    for (int i = areaStats.Modes.Length; i < 3; i++) {
+                        Logger.Log(LogLevel.Warn, "CrabbingAntidote", "Chapter " + areaStats.ID + " and side " + (i + 1) + " of save file " + (self.FileSlot + 1) + " was decrabbed");
+                        decrabbedStats[i] = new AreaModeStats();
+                    }
+                    areaStats.Modes = decrabbedStats;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Here is the list:
 - Aroxon Berry - a "no-midair-jump" winged strawberry, requires defining `ghost_aroxon_berry` and `aroxon_berry` in Sprites.xml to give it graphics
 - Badeline Hair Dye ([Reverse Card skin mod](https://gamebanana.com/mods/251787)) - gives Madeline's hair color to Badeline
 - [Bloom Command](https://gamebanana.com/mods/53703) - a mod that adds a `bloom` console command similar to the vanilla `lighting` command
+- Crabbing Antidote - a small mod that aims to fill missing C-side slots in save files to prevent file select from crashing, after a failed "Crab Sides" April Fools mod crabbed them :crab:
 - Cabob Berry Swap ([Frozen Heights](https://gamebanana.com/mods/150681)) - changes the GUI strawberry icon (now possible with [max480's Helping Hand](https://github.com/EverestAPI/ModResources/wiki/Helping-Hand-Extra-Features#reskinning-strawberries-in-menus))
 - [Display Message Command](https://gamebanana.com/mods/421935) - a mod that provides a simple command that displays arbitrary text on-screen, in the same style as the strawberry counter
 - Donker Berry Swap ([Solaris](https://gamebanana.com/mods/150559)) - same as the above but for another map :sweat_smile:
